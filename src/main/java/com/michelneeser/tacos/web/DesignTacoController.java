4e1a1e5621ad3680e453d2727e7cc76374816815
michelneeser <github@michelneeser.ch>
2019-10-21 22:34:00 +0200

    validation, WebConfig

--- src/main/java/com/michelneeser/tacos/web/DesignTacoController.java
+++ src/main/java/com/michelneeser/tacos/web/DesignTacoController.java
@@ -1,12 +1,17 @@
-package com.michelneeser.tacos;
+package com.michelneeser.tacos.web;
 
+import com.michelneeser.tacos.Ingredient;
+import com.michelneeser.tacos.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -42,7 +47,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
     }
--- src/main/java/com/michelneeser/tacos/web/OrderController.java
+++ src/main/java/com/michelneeser/tacos/web/OrderController.java
@@ -1,12 +1,15 @@
-package com.michelneeser.tacos;
+package com.michelneeser.tacos.web;
 
+import com.michelneeser.tacos.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +26,11 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
